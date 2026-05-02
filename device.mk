ec4a930372e109d2b8b510f12aa1feaa67b44348
rtyutechstudio <rtyutechstudio@icloud.com>
2026-05-02 14:56:23 +0800

    copied shared librarires

--- device.mk
+++ device.mk
@@ -1,6 +1,5 @@
 #
 # Copyright (C) 2022 The Android Open Source Project
-# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -11,3 +10,17 @@ PRODUCT_PACKAGES += \
     android.hardware.fastboot@1.0-impl-mock \
     android.hardware.fastboot@1.0-impl-mock.recovery \
     fastbootd
+# fbe
+TARGET_RECOVERY_DEVICE_MODULES += \
+    libpuresoftkeymasterdevice \
+    libcppbor_external \
+    libcppcose_rkp \
+    libsoft_attestation_cert
+
+TW_RECOVERY_ADDITIONAL_RELINK_LIBRARY_FILES += \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libpuresoftkeymasterdevice.so \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libcppbor_external.so \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libcppcose_rkp.so \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libsoft_attestation_cert.so
+
+
